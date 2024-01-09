f638ab93f9c7986412d85e743fb27550c7682074
heysaeid <heysaeid92@gmail.com>
2024-01-09 11:09:48 +0330

    ✨ Add BaseCache

---
+++ fastapi_and_caching/backends/base.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+
+
+class BaseCache(ABC):
+    
+    def __init__(self):
+        self.cache = None
+
+    @abstractmethod
+    async def init(self) -> None:
+        ...
+
+    @abstractmethod
+    async def close(self):
+        ...
+    
+    @abstractmethod
+    async def keys(self, key: str, prefix: str) -> list[str]:
+        ...
+
+    @abstractmethod
+    async def get(
+        self, 
+        key: str, 
+        prefix: str = None, 
+        params: dict = None,
+    ):
+        ...
+    
+    @abstractmethod
+    async def set(
+        self, 
+        key: str, 
+        value: str, 
+        expire: int = None,
+        prefix: str = None,
+        params: dict = None,
+        key_builder: callable = None,
+        **kwargs
+    ):
+        ...
+    
+    @abstractmethod
+    async def exists(self, key: str, prefix: str = None):
+        ...
+    
+    @abstractmethod
+    async def delete(self, key: str, prefix: str = None, params: dict = None): 
+        ...
+    
+    @abstractmethod
+    async def delete_startswith(
+        self, 
+        key: str, 
+        prefix: str = None, 
+        params: dict = None
+    ) -> None:
+        ...
